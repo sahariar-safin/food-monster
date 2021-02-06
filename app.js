c6382639ed95b0bd87da7ef0a844ecb9e9d552d7
Shafin The Developer <sahariar350@gmail.com>
2021-02-07 00:32:03 +0600

    Hip hip hurryyyy I am done

--- app.js
+++ app.js
@@ -6,33 +6,41 @@ search.addEventListener('click', function () {
         .then(data => {
             document.getElementById('foods').innerHTML = "";
             document.getElementById('foodsData').innerHTML = ' ';
-            const catagorys = document.getElementById('foods');
+            const foods = document.getElementById('foods');
             data.meals.forEach(element => {
-                const catagory = document.createElement('div')
-                catagory.innerHTML = `
-            <img src="${ element.strMealThumb }" onClick="handleCatagoryClick(${ element.idMeal })">
-            <h1 onClick="handleCatagoryClick(${ element.idMeal })" >${ element.strMeal }</h1>
+                const food = document.createElement('div')
+                food.innerHTML = `
+            <img src="${ element.strMealThumb }" onClick="handlefoodClick(${ element.idMeal })">
+            <h1 onClick="handlefoodClick(${ element.idMeal })" >${ element.strMeal }</h1>
             `;
-                catagory.className = "card";
-                catagorys.appendChild(catagory);
+                food.className = "card";
+                foods.appendChild(food);
             });
+        })
+        .catch(error => {
+            console.log(error);
+            document.getElementById('foods').innerHTML = "";
+            document.getElementById('foodsData').innerHTML = ' ';
+            const foods = document.getElementById('foods');
+            const notFound = document.createElement('h1')
+            notFound.innerHTML = `Sorry this item not found...Try again!`;
+            foods.appendChild(notFound);
         })
 })
 
-let handleCatagoryClick = foodId => {
+let handlefoodClick = foodId => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ foodId }`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.meals[0]);
-            let catagoryData = document.getElementById('foodsData');
+            let foodData = document.getElementById('foodsData');
             document.getElementById('foodsData').innerHTML = ' ';
             document.getElementById('foodsData').style.display = 'block';
-            let catagoryDetails = document.createElement('div')
-            catagoryDetails.innerHTML = `
+            let foodDetails = document.createElement('div')
+            foodDetails.innerHTML = `
             <img src="${ data.meals[0].strMealThumb }">
             <h1>${ data.meals[0].strMeal }</h1>
             <br>
-            <h1>Catagory: ${ data.meals[0].strCategory }</h1>
+            <h1>food: ${ data.meals[0].strCategory }</h1>
             <br>
             <h3>📌 ${ data.meals[0].strMeasure1 }</h3>
             <h3>📌 ${ data.meals[0].strMeasure2 }</h3>
@@ -41,7 +49,7 @@ let handleCatagoryClick = foodId => {
             <h3>📌 ${ data.meals[0].strMeasure5 }</h3>
             <h3>📌 ${ data.meals[0].strMeasure6 }</h3>
             `;
-            catagoryDetails.className = "catagory-details";
-            catagoryData.appendChild(catagoryDetails);
+            foodDetails.className = "food-details";
+            foodData.appendChild(foodDetails);
         })
 }
